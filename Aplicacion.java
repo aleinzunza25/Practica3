fae54da3ddd68bd2ef413374f80d206aa4219dce
edgardo <edgar_king1@hotmail.com>
2016-09-23 18:11:13 -0700

    crear el metodo main e imprimir

---
+++ Aplicacion.java
@@ -0,0 +1,14 @@
+package practica002;
+
+public class Aplicacion {
+
+	
+	public static void main(String[] args) {
+				
+		 Hora Relojuno=new Hora(4,24,32);
+		 System.out.println(Relojuno.toString());
+		 
+
+	}
+
+}
