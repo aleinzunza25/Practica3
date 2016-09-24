d113fcaa4d43b0230d925bbfbd0a874beb606096
lozpre <taller.sisope.2014@gmail.com>
2016-09-23 22:06:47 -0700

    mostrar un segundo reloj

--- Aplicacion.java
+++ Aplicacion.java
@@ -7,6 +7,9 @@ public class Aplicacion {
 				
 		 Hora Relojuno=new Hora(4,24,32);
 		 System.out.println(Relojuno.toString());
+		
+		 Hora Relojdos=new Hora(10,05,15);
+		 System.out.println(Relojuno.toString());
 		 
 
 	}
